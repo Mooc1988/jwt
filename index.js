b0600abf81cf43ba9b65043a77f8537e741ab5f9
frank <18652171101@163.com>
2016-06-14 12:44:51 +0800

    增加对过期token的处理,返回408

--- index.js
+++ index.js
@@ -1,63 +1,64 @@
-var assert   = require('assert');
-var thunkify = require('thunkify');
-var _JWT     = require('jsonwebtoken');
-var unless   = require('koa-unless');
-var util     = require('util');
+const thunkify = require('thunkify')
+const _JWT = require('jsonwebtoken')
+const unless = require('koa-unless')
+const util = require('util')
 
 // Make verify function play nice with co/koa
-var JWT = {decode: _JWT.decode, sign: _JWT.sign, verify: thunkify(_JWT.verify)};
+const JWT = {decode: _JWT.decode, sign: _JWT.sign, verify: thunkify(_JWT.verify)}
 
-module.exports = function(opts) {
-  opts = opts || {};
-  opts.key = opts.key || 'user';
+module.exports = function (opts) {
+  opts = opts || {}
+  opts.key = opts.key || 'user'
 
-  var tokenResolvers = [resolveCookies, resolveAuthorizationHeader];
+  const tokenResolvers = [resolveCookies, resolveAuthorizationHeader]
 
   if (opts.getToken && util.isFunction(opts.getToken)) {
-    tokenResolvers.unshift(opts.getToken);
+    tokenResolvers.unshift(opts.getToken)
   }
 
-  var middleware = function *jwt(next) {
-    var token, msg, user, parts, scheme, credentials, secret;
+  const middleware = function * jwt (next) {
+    let token, err, user, secret
 
-    for (var i = 0; i < tokenResolvers.length; i++) {
-      var output = tokenResolvers[i].call(this, opts);
+    for (let i = 0; i < tokenResolvers.length; i++) {
+      let output = tokenResolvers[i].call(this, opts)
 
       if (output) {
-        token = output;
-        break;
+        token = output
+        break
       }
     }
 
     if (!token && !opts.passthrough) {
-      this.throw(401, 'No authentication token found\n');
+      this.throw(401, 'No authentication token found\n')
     }
 
-    secret = (this.state && this.state.secret) ? this.state.secret : opts.secret;
+    secret = (this.state && this.state.secret) ? this.state.secret : opts.secret
     if (!secret) {
-      this.throw(500, 'Invalid secret\n');
+      this.throw(500, 'Invalid secret\n')
     }
 
     try {
-      user = yield JWT.verify(token, secret, opts);
-    } catch(e) {
-      msg = 'Invalid token' + (opts.debug ? ' - ' + e.message + '\n' : '\n');
+      user = yield JWT.verify(token, secret, opts)
+    } catch (e) {
+      err = e
     }
 
     if (user || opts.passthrough) {
-      this.state = this.state || {};
-      this.state[opts.key] = user;
-      yield next;
+      this.state = this.state || {}
+      this.state[opts.key] = user
+      yield next
     } else {
-      this.throw(401, msg);
+      // 针对过期token增加498 code
+      let code = err.name === 'TokenExpiredError' ? 408 : 401
+      let msg = 'Invalid token' + (opts.debug ? ' - ' + err.message + '\n' : '\n')
+      this.throw(code, msg)
     }
-  };
+  }
 
-  middleware.unless = unless;
+  middleware.unless = unless
 
-  return middleware;
-};
-
+  return middleware
+}
 
 /**
  * resolveAuthorizationHeader - Attempts to parse the token from the Authorization header
@@ -69,27 +70,26 @@ module.exports = function(opts) {
  * @param  {object}      opts The middleware's options
  * @return {String|null}      The resolved token or null if not found
  */
-function resolveAuthorizationHeader(opts) {
+function resolveAuthorizationHeader (opts) {
   if (!this.header || !this.header.authorization) {
-    return;
+    return
   }
 
-  var parts = this.header.authorization.split(' ');
+  let parts = this.header.authorization.split(' ')
 
   if (parts.length === 2) {
-    var scheme = parts[0];
-    var credentials = parts[1];
+    let scheme = parts[0]
+    let credentials = parts[1]
 
     if (/^Bearer$/i.test(scheme)) {
-      return credentials;
+      return credentials
     }
   } else {
     if (!opts.passthrough) {
-      this.throw(401, 'Bad Authorization header format. Format is "Authorization: Bearer <token>"\n');
+      this.throw(401, 'Bad Authorization header format. Format is "Authorization: Bearer <token>"\n')
     }
   }
 }
-
 
 /**
  * resolveCookies - Attempts to retrieve the token from a cookie
@@ -101,13 +101,13 @@ function resolveAuthorizationHeader(opts) {
  * @param  {object}      opts This middleware's options
  * @return {String|null}      The resolved token or null if not found
  */
-function resolveCookies(opts) {
+function resolveCookies (opts) {
   if (opts.cookie && this.cookies.get(opts.cookie)) {
-    return this.cookies.get(opts.cookie);
+    return this.cookies.get(opts.cookie)
   }
 }
 
 // Export JWT methods as a convenience
-module.exports.sign   = _JWT.sign;
-module.exports.verify = _JWT.verify;
-module.exports.decode = _JWT.decode;
+module.exports.sign = _JWT.sign
+module.exports.verify = _JWT.verify
+module.exports.decode = _JWT.decode
